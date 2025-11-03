0f20ac5d24c9f2ff4f944851d3d1ce0f6e13389c
Paramdeep Singh <psingh20@rrc.ca>
2025-11-02 23:14:46 -0600

    updated these files for correct working

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express, { Express } from "express";
+import loanRoutes from "./api/v1/routes/loanRoutes";
+import errorHandler from "./api/v1/middleware/errorHandler";
+
 
 // Initialize Express application
 const app: Express = express();
+app.use(express.json());
 
 // Define a route
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
+
+app.use("/api/v1/loans", loanRoutes);
+app.use(errorHandler);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import app from "./app";
 import { Server } from "http";
+import loanRoutes from "./api/v1/routes/loanRoutes";
+import  errorHandler from "./api/v1/middleware/errorHandler";
+
+// Root test route
+app.get("/", (_req, res) => res.send("API is working!"));
+
+// loan routes
+app.use("/api/v1/loans", loanRoutes);
+
+app.use(errorHandler);
 
 const PORT: string | number = process.env.PORT || 3000;
 
 const server: Server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+
+
 });
 
 export { server };
