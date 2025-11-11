349e8c168672229717fdb2c7c4bd7bf53b319f74
Paramdeep Singh <psingh20@rrc.ca>
2025-11-11 11:49:25 -0600

    fixed it  did mistake while adding firebase-service-account.json to .gitignore

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -7,13 +7,40 @@ import {
   getLoanById,
   deleteLoan,
 } from "../controllers/loanController";
+import authenticate from "../middleware/authenticate"; 
+import { authorize } from "../middleware/authorize";
+
 
 const router = Router();
-router.get("/", getAllLoans);
-router.post("/", createLoan)
-router.put("/:id/review", reviewLoan);
-router.put("/:id/approve", approveLoan);
-router.delete("/:id", deleteLoan);
-router.get("/:id", getLoanById);
+router.get(
+  "/",
+  authenticate,
+  authorize({ hasRole: ["officer", "reviewer", "admin"] }), getAllLoans);
+
+router.post(
+  "/",
+  authenticate,
+  authorize({ hasRole: ["officer", "admin"] }),
+   createLoan)
+
+router.put("/:id/review",
+   authenticate,
+  authorize({ hasRole: ["reviewer", "officer", "admin"] }),
+   reviewLoan);
+
+router.put(
+  "/:id/approve",
+   authenticate,
+   authorize({ hasRole: ["admin"] }),
+   approveLoan);
+
+router.delete(
+  "/:id",
+   authenticate,
+  authorize({ hasRole: ["admin"] }), deleteLoan);
+  
+router.get("/:id",
+  authenticate,
+  authorize({ hasRole: ["officer", "reviewer", "admin"] }), getLoanById);
 
 export default router;
